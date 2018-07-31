9b6f3460cabd26dfc93c56cab8a4766aaaf291fa
Isaiah Cruz <isaiahgcruz@gmail.com>
2018-07-31 11:33:44 +0800

    Update eslint rule to prefer es6 classes over stateless function

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,8 @@ module.exports = {
     browser: true,
   },
   rules: {
-    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/prefer-es6-class': [1, 'always'],
+    'react/prefer-stateless-function': false,
   },
 };
